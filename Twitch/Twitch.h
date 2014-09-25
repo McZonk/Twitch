89a604341ceb0840bd4ac349515a73c3337c7d51
Max <mczonk@gmail.com>
2014-09-25 08:09:53 +0200

    Initial Commit

---
+++ Twitch/Twitch.h
@@ -0,0 +1,19 @@
+//
+//  Twitch.h
+//  Twitch
+//
+//  Created by Maximilian Christ on 25/09/14.
+//  Copyright (c) 2014 McZonk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Twitch.
+FOUNDATION_EXPORT double TwitchVersionNumber;
+
+//! Project version string for Twitch.
+FOUNDATION_EXPORT const unsigned char TwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Twitch/PublicHeader.h>
+
+
